4dd038feea5b6e001f5624606508f9410987adf9
dagger57 <dagger57@dagger57-VirtualBox.(none)>
2012-09-25 19:15:40 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '4ba5ee7afe591f7af77e4bd1226bd26d6a34472e6ebef3ad91f0452a40a4cd22ebc6a8a68c6ccd2f70817a7a0adb1a5612aeca9298e05619a4548c6be208a299'
